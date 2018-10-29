d13b4ef2c63f8a27f5a8053c4febe215ff6cb62a
DUUUUUUUDE <42291216+DUUUUUUUDE@users.noreply.github.com>
2018-10-29 17:19:49 +0100

    LOL SO RANDOM

---
+++ Editer/Assets/Editor/SpawnSphereWindow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SpawnSphereWindow : EditorWindow
+{
+
+    string SphereName = "SphereChan";
+
+    Vector3 Pos;
+
+    PrimitiveType type;
+
+
+    [MenuItem ("Window/SuperChan")]
+    public static void ShowWindow ()
+    {
+        GetWindow<SpawnSphereWindow>("SuperChan");
+    }
+
+    private void OnGUI()
+    {
+        EditorGUILayout.Space();
+        type = (PrimitiveType)EditorGUILayout.EnumFlagsField("Mesh",type);
+        EditorGUILayout.Space ();
+        Pos = EditorGUILayout.Vector3Field("SphereChan Pos", Pos);
+        EditorGUILayout.Space();
+
+        if (GUILayout.Button ("CREATE " + type + "-CHAN !!"))
+        {
+            GameObject sphere = GameObject.CreatePrimitive(type);
+            sphere.transform.position = Pos;
+        }
+
+    }
+
+}
